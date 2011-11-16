a76744cdb24e87324e3f207b8c303c6c61c34e7a
niels at home <ncoelle@gmx.de>
2011-11-16 07:24:39 +0100

    Überflüssigen Kommentar entfernt

--- 20111107/src/main/java/de/neuesausfreaktown/yahtzeekata/RankCalculator.java
+++ 20111107/src/main/java/de/neuesausfreaktown/yahtzeekata/RankCalculator.java
@@ -1,9 +1,5 @@
 package de.neuesausfreaktown.yahtzeekata;
 
-/**
- * Created by IntelliJ IDEA. User: coellen Date: 07.11.11 Time: 22:03 To change this template use File | Settings | File
- * Templates.
- */
 public interface RankCalculator {
     int calculateScore(int... pips);
 }
